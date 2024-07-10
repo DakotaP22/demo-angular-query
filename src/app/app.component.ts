d611fbd4b66398d50e30d69ef1aa83d01dd01860
DakotaP22 <64506141+DakotaP22@users.noreply.github.com>
2024-07-09 20:43:28 -0400

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    MatSidenavModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatSlideToggleModule,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'AngularTemplateM3';
+}
