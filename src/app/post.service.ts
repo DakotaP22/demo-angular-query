4de6b9627833209b5a862d6a4db5802f4272d8b1
DakotaP22 <peased22@gmail.com>
2024-07-09 22:19:16 -0400

    feat: add post service with functionality to get posts and comments

---
+++ src/app/post.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable, inject } from "@angular/core";
+import { lastValueFrom } from "rxjs";
+
+export type Post = {
+    userId: number,
+    id: number,
+    title: string,
+    body: string
+}
+
+export type Comment = {
+    postId: number,
+    id: number,
+    name: string,
+    email: string,
+    body: string
+}
+
+
+@Injectable()
+export class PostService {
+    private readonly http = inject(HttpClient);
+
+    getPosts(): Promise<Post[]> {
+        return lastValueFrom(this.http.get<Post[]>(`https://jsonplaceholder.typicode.com/posts`))
+    }
+
+    getCommentForPost(postId: number) {
+        return lastValueFrom(
+            this.http.get<Comment[]>(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`)
+        )
+    }
+
+}
